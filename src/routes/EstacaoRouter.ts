2edf2382d44e5b98150499ddc381ea2f83db111b
rafawaltrick <rafael.waltrick@fatec.sp.gov.br>
2023-03-22 10:34:51 -0300

    estacao buscaId e estacao Edicao

--- src/routes/EstacaoRouter.ts
+++ src/routes/EstacaoRouter.ts
@@ -4,8 +4,9 @@ import EstacaoController from "../controllers/EstacaoController";
 const routes = Router();
 
 routes.get('/buscar', EstacaoController.buscarEstacoes);
-
+routes.get('/buscar/:id', EstacaoController.buscarEstacaoPorId);
 routes.post('/cadastrar', EstacaoController.cadastrarEstacao);
+routes.put('/editar/:id', EstacaoController.editarEstacao);
 routes.delete('/excluir/:id', EstacaoController.excluirEstacao);
 
 export default routes;
