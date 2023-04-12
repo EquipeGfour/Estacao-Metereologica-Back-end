75f2c07c82e14f53ad1074c4d2d029f328470d63
Raniel <raniel.paula@fatec.sp.gov.br>
2023-04-12 10:44:00 -0300

    Criado Cadastro e Exclusão do controller "Usuario"

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -25,6 +25,10 @@ class UsuarioController{
 
     public async cadastrarUsuario(req: Request, res: Response) {
         try{
+            const {nome, email, senha, tipo_usuario} = req.body
+            const usuario = await db.getRepository(Usuario).create({nome, email, senha, tipo_usuario});
+            await db.getRepository(Usuario).save(usuario);
+            res.json(usuario)
 
         }catch(error){
             console.log(error);
@@ -41,12 +45,17 @@ class UsuarioController{
         }
     };
 
-    public async excluirUsuario(req: Request, res:Response) {
+    public async excluirUsuario(req: Request, res: Response) {
         try{
-
+            const usuario = await db.getRepository(Usuario).findOneBy({id: Number(req.params.id)});
+            if(usuario){
+                await db.getRepository(Usuario).delete(usuario)
+                res.json(`Usuário de id ${usuario.id} excluida com sucesso...`)
+            }else{
+                res.json(`Usuário de id ${req.params.id} não encontrada...`)
+            }
         }catch(error){
-            console.log(error);
-            res.status(500).json({message: error});            
+            res.status(500).json({ message: error });            
         }
     };
 }
--- src/routes/UsuarioRouter.ts
+++ src/routes/UsuarioRouter.ts
@@ -7,6 +7,6 @@ routes.get('/buscar', UsuarioController.buscarUsuarios);
 routes.get('/buscar/:id', UsuarioController.buscarUsuariosPorId);
 routes.post('/cadastrar', UsuarioController.cadastrarUsuario);
 routes.put('/editar/:id', UsuarioController.editarUsuario);
-routes.delete('/excluir/:id', UsuarioController.editarUsuario);
+routes.delete('/excluir/:id', UsuarioController.excluirUsuario);
 
 export default routes
