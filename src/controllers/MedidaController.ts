d1a2280e2802f179808a670025eacda7d0bc5208
Raniel <raniel.paula@fatec.sp.gov.br>
2023-04-14 11:50:41 -0300

    Criado estrutura do controller de "Medidas"

---
+++ src/controllers/MedidaController.ts
@@ -0,0 +1,53 @@
+import db from "../config/db";
+import { Request, Response } from "express";
+import Medidas from "../models/Medida";
+import { log } from "console";
+
+class MedidaController{
+    public async buscarMedidas (req:Request, res: Response){
+        try{
+
+        }catch(error){
+            console.log(error);
+            res.status(500).json({message: error});            
+        }
+    };
+
+    public async buscarMedidasPorId(req: Request, res: Response){
+        try{
+
+        }catch(error){
+            console.log(error);
+            res.status(500).json({message: error});            
+        }
+    };
+
+    public async cadastrarMedidas(req: Request, res: Response){
+        try{
+
+        }catch(error){
+            console.log(error);
+            res.status(500).json({message: error});            
+        }
+    };
+
+    public async editarMedidas(req:Request, res: Response){
+        try{
+
+        }catch(error){
+            console.log(error);
+            res.status(500).json({message: error});            
+        }
+    };
+
+    public async excluirMedidas(req: Request, res: Response){
+        try{
+
+        }catch(error){
+            console.log(error);
+            res.status(500).json({message: error});            
+        }
+    }
+}
+
+export default MedidaController
